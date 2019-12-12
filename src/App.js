983fc7c31e8153c7403c7f39ca5ec4828388ba54
Karlo Marinovic <36795522+ikaem@users.noreply.github.com>
2019-12-12 19:28:14 +0100

    app.js to class

--- src/App.js
+++ src/App.js
@@ -2,7 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer s"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +46,6 @@ function App() {
       </header>
     </div>
   );
-}
+} */
 
 export default App;
