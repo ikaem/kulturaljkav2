dc5ca0501b07e655c9e02fedcf9dfc8058c63b30
Karlo Marinovic <36795522+ikaem@users.noreply.github.com>
2019-12-19 21:53:46 +0100

    Updates

---
+++ precache-manifest.1a343ba5557f0b0803ab5c61e37a6bf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056d326739920592479c7001b0134ab3",
+    "url": "/ikaem/kulturaljkav2.git/index.html"
+  },
+  {
+    "revision": "a852f0f2cdc1ec0241a1",
+    "url": "/ikaem/kulturaljkav2.git/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "aed8064433e5ca357054",
+    "url": "/ikaem/kulturaljkav2.git/static/css/main.07e2f46e.chunk.css"
+  },
+  {
+    "revision": "a852f0f2cdc1ec0241a1",
+    "url": "/ikaem/kulturaljkav2.git/static/js/2.7e63720b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ikaem/kulturaljkav2.git/static/js/2.7e63720b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aed8064433e5ca357054",
+    "url": "/ikaem/kulturaljkav2.git/static/js/main.abc70cee.chunk.js"
+  },
+  {
+    "revision": "339a913224bdecd572a1",
+    "url": "/ikaem/kulturaljkav2.git/static/js/runtime-main.93330cf2.js"
+  }
+]);
